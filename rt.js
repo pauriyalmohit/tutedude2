35700847b01cd29bff25aa8c1716a3a4fa37f3dc
mohit pauriyal <mohitpouriyalmonu7088@gmail.com>
2025-07-15 14:13:59 +0530

    modified:   index.html

--- rt.js
+++ rt.js
@@ -1,33 +1,44 @@
-var start =new Date().getTime();
-function getRandomcolor() {
-    var letters = '0123456789ABCDEF';
-    var color="#";
-    for (var i=0; i<6; i++){
-        color += letters[Math.floor(Math.random()*16)];
-    }
- return color;
-}
-function move(){
-    var left;
-    var top;
-    var wh;
-    left= Math.random() *400;
-    right=Math.random() *400;
-    wh=((Math.random() *400) +100);
-    document.getElementById("shape").style.left=left;
-    document.getElementById("shape").style.top=top;
-    document.getElementById("shape").style.width=wh;
-    document.getElementById("shape").style.height=wh;
-    document.getElementById("shape").style.display="block";
-    document.getElementById("shape").style.backgroundColor=getRandomcolor();
-    start=new Date().getTime();
-}
-move();
-document.getElementById("shape").onclick = function(){
-    document.getElementById("shape").style.display="none";
-    var end=new Date().getTime();
-    var timeTaken=(end-start)/1000;
-    alert(timeTaken);
-    move();
-}
+  var start;
 
+    function getRandomColor() {
+      var letters = '0123456789ABCDEF';
+      var color = '#';
+      for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+      }
+      return color;
+    }
+
+    function moveShape() {
+      var shape = document.getElementById("shape");
+
+      var left = Math.random() * (window.innerWidth - 150);
+      var top = Math.random() * (window.innerHeight - 150);
+      var size = Math.random() * 100 + 50;
+
+      shape.style.left = left + "px";
+      shape.style.top = top + "px";
+      shape.style.width = size + "px";
+      shape.style.height = size + "px";
+      shape.style.backgroundColor = getRandomColor();
+      shape.style.display = "block";
+
+      start = new Date().getTime();
+    }
+
+   /* function appearAfterDelay() {
+      var delay = Math.random() ; // 1 to 3 seconds
+      setTimeout(moveShape, delay);
+    }*/
+
+    document.getElementById("shape").onclick = function () {
+      var end = new Date().getTime();
+      var timeTaken = (end - start);
+
+      alert("Your reaction time: " + timeTaken + " seconds");
+
+      this.style.display = "none";
+      appearAfterDelay();
+    };
+
+    appearAfterDelay();
